9ac934ac54a545c23dadee1c558030d7289a3ac9
Danielvartatt <daniel1.kodehode@gmail.com>
2025-01-22 19:50:18 +0100

    med kontroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { meny } from "./Menydata.js";
 import Dish from "./assets/Dish.jsx";
-import MenyList from "./Meny.jsx";
 function App() {
   return (
     <div>
       {meny.map((meny, id) => {
         return <Dish key={id} data={meny} />;
       })}
-      <div>
-        {meny.map((meny, id) => {
-          return <MenyList key={id} data={meny} />;
-        })}
-      </div>
+      <div></div>
     </div>
   );
 }
